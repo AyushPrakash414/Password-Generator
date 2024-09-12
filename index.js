4042733484c2b09426aafced5f084767c5c993c8
AyushPrakash414 <157799074+AyushPrakash414@users.noreply.github.com>
2024-09-13 01:11:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const passwordBox = document.getElementById("password");
-const length = 12;
+const vari = document.querySelector("#variable");
+let length;
+
+
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number = "1234567890";
@@ -8,12 +11,23 @@ const allChar = upperCase + lowerCase + symbol;
 
 function createPassword() {
     let password = "";
-    password += upperCase[Math.floor(Math.random() * upperCase.length)];
-    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-    password += symbol[Math.floor(Math.random() * symbol.length)];
+    if(vari.value!=0){
+        length=vari.value;
+    }
+    else{
+        length=5;
+    }
+    
 
     while (length > password.length) {
         password += allChar[Math.floor(Math.random() * allChar.length)];
     }
     passwordBox.value = password;
+}
+function copyText() {
+    const textToCopy = passwordBox.value;
+
+    navigator.clipboard.writeText(textToCopy).then(() => {
+        alert("Text copied: " + textToCopy);
+    })
 }
